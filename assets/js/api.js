4f5ae50b0cdacacd4ed676236e9e8369af7c9774
hessennasser <hessenaloosh@gmail.com>
2023-04-16 16:36:22 +0200

    the end of project <3

---
+++ assets/js/api.js
@@ -0,0 +1,14 @@
+export async function fetchData(url, successCallback, errorCallback) {
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        successCallback(data)
+    }
+    else {
+        const error = await response.json();
+        errorCallback && errorCallback(data)
+    }
+
+}
